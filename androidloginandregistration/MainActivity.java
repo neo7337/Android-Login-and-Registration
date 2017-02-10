86719fddd535bf7c83c7f0aef8ec814329551f4a
Aditya <adityak007mr@gmail.com>
2017-02-10 23:29:05 +0530

    Update MainActivity.java

--- androidloginandregistration/MainActivity.java
+++ androidloginandregistration/MainActivity.java
@@ -38,7 +38,9 @@ public class MainActivity extends ActionBarActivity {
     protected EditText username;
     private EditText password;
     protected String enteredUsername;
-    private final String serverUrl = "http://192.168.43.72/androidlogin/index.php";
+    //this is the location of your index.php file 
+    //use the ipconfig command on your cmd to get the ipv4 address and use that address in place of the XXX.XXX.XXX.XXX
+    private final String serverUrl = "http://XXX.XXX.XXX.XXX/androidlogin/index.php";  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
