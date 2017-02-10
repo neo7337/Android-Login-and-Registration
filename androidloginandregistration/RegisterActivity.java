8df367cf1c3c2a22012f702abd1a559899d0c65e
Aditya <adityak007mr@gmail.com>
2017-02-10 22:03:04 +0530

    Add files via upload

---
+++ androidloginandregistration/RegisterActivity.java
@@ -0,0 +1,160 @@
+package inducesmile.com.androidloginandregistration;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RegisterActivity extends ActionBarActivity {
+
+    protected EditText username;
+    private EditText password;
+    private EditText email;
+    protected String enteredUsername;
+    private final String serverUrl = "http://192.168.43.72/androidlogin/index.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = (EditText)findViewById(R.id.username_field);
+        password = (EditText)findViewById(R.id.password_field);
+        email = (EditText)findViewById(R.id.email_field);
+        Button signUpButton = (Button)findViewById(R.id.sign_up);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enteredUsername = username.getText().toString();
+                String enteredPassword = password.getText().toString();
+                String enteredEmail = email.getText().toString();
+
+                if(enteredUsername.equals("") || enteredPassword.equals("") || enteredEmail.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Username or password or email must be filled", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(enteredUsername.length() <= 1 || enteredPassword.length() <= 1){
+                    Toast.makeText(RegisterActivity.this, "Username or password length must be greater than one", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // request authentication with remote server4
+                AsyncDataClass asyncRequestObject = new AsyncDataClass();
+                asyncRequestObject.execute(serverUrl, enteredUsername, enteredPassword, enteredEmail);
+            }
+        });
+    }
+
+    private class AsyncDataClass extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpParams httpParameters = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParameters, 5000);
+            HttpConnectionParams.setSoTimeout(httpParameters, 5000);
+
+            HttpClient httpClient = new DefaultHttpClient(httpParameters);
+            HttpPost httpPost = new HttpPost(params[0]);
+
+            String jsonResult = "";
+            try {
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("username", params[1]));
+                nameValuePairs.add(new BasicNameValuePair("password", params[2]));
+                nameValuePairs.add(new BasicNameValuePair("email", params[3]));
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                HttpResponse response = httpClient.execute(httpPost);
+                jsonResult = inputStreamToString(response.getEntity().getContent()).toString();
+                System.out.println("Returned Json object " + jsonResult.toString());
+
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonResult;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            System.out.println("Resulted Value: " + result);
+            if(result.equals("") || result == null){
+                Toast.makeText(RegisterActivity.this, "Server connection failed", Toast.LENGTH_LONG).show();
+                return;
+            }
+            int jsonResult = returnParsedJsonObject(result);
+            if(jsonResult == 0){
+                Toast.makeText(RegisterActivity.this, "Invalid username or password or email", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if(jsonResult == 1){
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                intent.putExtra("USERNAME", enteredUsername);
+                intent.putExtra("MESSAGE", "You have been successfully Registered");
+                startActivity(intent);
+            }
+        }
+        private StringBuilder inputStreamToString(InputStream is) {
+            String rLine = "";
+            StringBuilder answer = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            try {
+                while ((rLine = br.readLine()) != null) {
+                    answer.append(rLine);
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return answer;
+        }
+    }
+    private int returnParsedJsonObject(String result){
+
+        JSONObject resultObject = null;
+        int returnedResult = 0;
+        try {
+            resultObject = new JSONObject(result);
+            returnedResult = resultObject.getInt("success");
+            System.out.println(returnedResult);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return returnedResult;
+    }
+}
